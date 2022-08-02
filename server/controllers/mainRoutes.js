6f9e37fca50bd7820744ec497928e5ea9444d530
Howie Miao <howie.miao@gmail.com>
2022-08-02 11:35:04 -0600

    encryption start

--- server/controllers/mainRoutes.js
+++ server/controllers/mainRoutes.js
@@ -1,8 +1,40 @@
 const router = require("express").Router();
 const { Newsletter, Login } = require("../Schema")
 var bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs')
+require('dotenv').config()
+const saltRounds = parseInt(process.env.SALT)
 router.use(bodyParser.json()); 
 router.use(bodyParser.urlencoded({ extended: true })); 
+
+
+async function hash (toHash) {
+
+    const hashed = await new Promise((resolve, reject) => {
+      bcrypt.hash(toHash, saltRounds, function(err, hash) {
+        if (err) reject(err)
+        resolve(hash)
+      });
+    })
+    
+    console.log(hashed)
+    return hashed
+}
+
+async function compareHash (plain,hashed) {
+
+    const bool = await new Promise((resolve, reject) => {
+      bcrypt.compare(plain, hashed, function(err, result) {
+        if (err) reject(err)
+        if(result){resolve(true)}
+        else{resolve(false)}
+      });
+    })
+    
+    console.log(bool)
+    return bool
+}
+
 
 router 
     .route("/customer-service")
@@ -26,8 +58,10 @@ router
     .route("/newsletter")
     .post(async (req,res)=>{
         const { name, email, phone } = req.body
-        await Newsletter.create( { name: name, email: email, phone: phone})
-        res.send("data passed");
+        let temp = await hash(email)
+        await Newsletter.create( { name: name, email: temp, phone: phone})
+        await compareHash(email,temp)
+        res.send(`${hash(name)}`);
     })
 
 module.exports = router
