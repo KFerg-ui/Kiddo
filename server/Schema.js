8de336841d37a61014520f755e75283e6c6e648b
Howie Miao <howie.miao@gmail.com>
2022-08-02 12:51:21 -0600

    started web tokens

--- server/Schema.js
+++ server/Schema.js
@@ -23,6 +23,7 @@ const Newsletter = mongoose.model("newsletter", NewsletterSchema)
 const LoginSchema = new mongoose.Schema({
     name: { type : String , required : true },
     email: { type : String , unique : true, required : true },
+    password: { type: String, required : true},
     phone: String,
     address: String,
     business: String,
