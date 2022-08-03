255c79d005060b3603b5631c7bd1f719746ef3d8
Kyle Ferguson <ferguson.kd@gmail.com>
2022-08-03 16:59:32 -0400

    added grid to home, with styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,31 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import Login from './components/login/Login';
-import Register from './components/register/Register';
-import InvestorPortal from './components/investor/InvestorPortal';
-import CustomerServicePortal from './components/support/CustomerServicePortal';
-import CompanyInfo from './components/info/CompanyInfo';
-import AdminLogin from './components/admin/AdminLogin';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import InvestorPortal from "./components/investor/InvestorPortal";
+import CustomerServicePortal from "./components/support/CustomerServicePortal";
+import CompanyInfo from "./components/info/CompanyInfo";
+import AdminLogin from "./components/admin/AdminLogin";
 
 function App() {
   return (
+    <Router>
+      <Navbar />
 
-<Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/investors" element={<InvestorPortal />} />
+        <Route path="/support" element={<CustomerServicePortal />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/info" element={<CompanyInfo />} />
+      </Routes>
 
-<Navbar/>
-
-
- <Routes>
-
-<Route path="/" element={<Home /> } />
-<Route path="/register" element={<Register /> } />
-<Route path="/login" element={<Login /> } />
-<Route path="/investors" element={<InvestorPortal /> } />
-<Route path="/support" element={<CustomerServicePortal /> } />
-<Route path="/admin" element={<AdminLogin /> } />
-<Route path="/info" element={<CompanyInfo /> } />
-
-
-
- </Routes>
-
-<Footer />
-
- </Router>
+      <Footer />
+    </Router>
   );
 }
 
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,59 +1,126 @@
-import React from 'react'
-import './Home.css'
-import video from '../../assets/animation.mp4'
-import image1 from '../../assets/image-1.png'
-import image2 from '../../assets/image-2.png'
-import image3 from '../../assets/image-3.png'
-import image4 from '../../assets/image-4.png'
-import image5 from '../../assets/image-5.png'
-import image7 from '../../assets/image-7.png'
+import React from "react";
+import "./Home.css";
+import video from "../../assets/animation.mp4";
+import image1 from "../../assets/image-1.png";
+import image2 from "../../assets/image-2.png";
+import image3 from "../../assets/image-3.png";
+import image4 from "../../assets/image-4.png";
+import image5 from "../../assets/image-5.png";
+import image7 from "../../assets/image-7.png";
+import { Grid } from "@mui/material";
+
 const Home = () => {
   return (
-    <div className='home'>
-    <div className='container'>
-      <h1 className='title'>Home</h1>
+    <Grid container className="gridHomeContainer" justifyContent="center">
+      <Grid item className="animation" xs={10}>
+        <video className="videoBox" width="100%" height="100%" controls>
+          <source src={video} id="vid1" type="video/mp4" />
+        </video>
+      </Grid>
+      <Grid item className="missionGrid" xs={10}>
+        <h1 className="title">Welcome To Kiddo!</h1>
+        <div className="register">
+          <button className="register-button">Register Here</button>
+        </div>
 
+        <div className="mission-statement">
+          <h1 className="missionHead">Our Mission</h1>
+          <p width="100%" className="missionText">
+            Kiddo is a new social network for children. Through a thorough
+            understanding of the existing social media ecosystem we aim to
+            develop Kiddo with fundamentally different values and priorities:
+            Our users are our customers, not our products. Kiddo will be
+            ad-free, relying on small monthly subscription charges instead of
+            advertisements. This alone removes most of the structural incentives
+            for unhealthy social network features. Our goal is to maximize child
+            development and entertainment, not screen time. Kiddo recognizes it
+            should be a small part of a much larger non-screen life for kids. We
+            will rely on the insights of recognized childhood development
+            experts to ensure an enriching and supportive environment.Our
+            workers and users direct the future of Kiddo. Kiddo’s ownership and
+            day-to-day decisions will be operated as a worker cooperative, with
+            any outside investors having equity but no voice in governance.
+            Users will have a voice on the future of Kiddo via elected,
+            volunteer advisory boards.
+          </p>
+        </div>
+      </Grid>
+      <Grid
+        container
+        className="infoBoxes"
+        xs={12}
+        justifyContent="space-evenly"
+        sx={{ flexDirection: { md: "row" } }}
+      >
+        <Grid
+          container
+          className="infoOne"
+          justifyContent="space-evenly"
+          md={5}
+          xs={9}
+        >
+          <Grid item className="imageOne" width="50%" xs={5}>
+            <img src={image1} id="img1" alt="first image" width="100%" />
+          </Grid>
+          <Grid item className="infoText1" xs={5}>
+            <p>information lorem lorem lorem</p>
+          </Grid>
+        </Grid>
 
-      <video width="750" height="500" controls >
-      <source src={video} id="vid1" type="video/mp4"/>
-     </video>
+        <Grid
+          container
+          className="infoOne"
+          md={5}
+          xs={9}
+          justifyContent="space-evenly"
+        >
+          <Grid item className="imageOne" width="50%" xs={5}>
+            <img src={image2} id="img2" alt="second image" width="100%" />
+          </Grid>
+          <Grid item className="infoText2" xs={5}>
+            <p>information lorem lorem lorem</p>
+          </Grid>
+        </Grid>
 
-<div className='register'>
-    <button className='register-button'>Register Here</button>
-  
-</div>
+        <Grid
+          container
+          className="infoOne"
+          md={5}
+          xs={9}
+          justifyContent="space-evenly"
+        >
+          <Grid item className="imageOne" width="50%" xs={5}>
+            <img src={image3} id="img3" alt="third image" width="100%" />
+          </Grid>
+          <Grid item className="infoText2" xs={5}>
+            <p>information lorem lorem lorem</p>
+          </Grid>
+        </Grid>
 
-<div className='mission-statement'>
-  <h1>Our Mission</h1>
-  <p>Kiddo is a new social network for children. Through a thorough understanding of the existing social media ecosystem we aim to develop Kiddo with fundamentally different values and priorities:
-Our users are our customers, not our products. Kiddo will be ad-free, relying on small monthly subscription charges instead of advertisements. This alone removes most of the structural incentives for unhealthy social network features.
-Our goal is to maximize child development and entertainment, not screen time. Kiddo recognizes it should be a small part of a much larger non-screen life for kids. We will rely on the insights of recognized childhood development experts to ensure an enriching and supportive environment.Our workers and users direct the future of Kiddo. Kiddo’s ownership and day-to-day decisions will be operated as a worker cooperative, with any outside investors having equity but no voice in governance. Users will have a voice on the future of Kiddo via elected, volunteer advisory boards.</p>
-</div>
+        <Grid
+          container
+          className="infoOne"
+          md={5}
+          xs={9}
+          justifyContent="space-evenly"
+        >
+          <Grid item className="imageOne" width="50%" xs={5}>
+            <img src={image7} id="img7" alt="ads image" width="100%" />
+          </Grid>
+          <Grid item className="infoText2" xs={5}>
+            <p>information lorem lorem lorem</p>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item className="newsies" xs={10}>
+        <form className="newsForm">
+          <input type="text" placeholder="name"></input>
+          <input type="text" placeholder="email"></input>
+          <button>Sign Up</button>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
 
-
-
-  <div className='images'>
-<img src={image1} id="img1" alt="first image" />
-<img src={image2} id="img2" alt="second image" />
-<img src={image3} id="img3" alt="third image" />
-<img src={image7} id="img7" alt="ads image" />
-
-</div>
-
-<div className='newsletter'>
-<form>
-  <input type="text" placeholder='name'></input>
-  <input type="text" placeholder='email'></input>
-  <button>Sign Up</button>
-  
-   </form>
-
-</div>
-
-</div>
-</div>
-   
-  )
-}
-
-export default Home
+export default Home;
