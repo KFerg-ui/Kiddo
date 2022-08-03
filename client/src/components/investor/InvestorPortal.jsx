16653d627841d26a61cb774230b00aad9a2333c9
David Roos <thanks4nospam@gmail.com>
2022-08-03 14:24:50 -0400

    mocked up 3 wireframes and light css styling

--- client/src/components/investor/InvestorPortal.jsx
+++ client/src/components/investor/InvestorPortal.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-import './InvestorPortal.css'
+import React from "react";
+import "./InvestorPortal.css";
+import image8 from "../../assets/image-8.png";
+import image9 from "../../assets/image-9.png";
+import { Grid } from "@mui/material";
+
 const InvestorPortal = () => {
   return (
-    <div>InvestorPortal</div>
-  )
-}
+    <div>
+      <div>
+        {" "}
+        <Grid item xs={11} className="welcomeTitle" direction="row">
 
-export default InvestorPortal
+        <h1>Welcome (Investment Company/Name)</h1>
+        </Grid>
+      </div>
+
+      <Grid container className="gridWrapContainer" direction="row">
+        <Grid item xs={11} className="picWrap pic8Wrap" direction="row">
+          <Grid item xs={5} className="bulletPhrases">
+            Kiddo information <br/>
+            Market Research <br/>            
+            Business model 
+          </Grid> 
+          <Grid item xs={5} className="picContainer">
+            <img className='image8' src={image8} id="img8" alt="4 cartoon kids at a table" /></Grid>
+        </Grid>
+      </Grid>
+      
+      <Grid container className="gridWrapContainer" direction="row">
+        <Grid item xs={11} className="picWrap pic9Wrap" direction="row">
+          <Grid item xs={5} className="picContainer">
+            <img className="image9" src={image9} id="img9" alt="cartoon investor at computer " /></Grid> 
+          <Grid item xs={5} className="bulletPhrases">
+            Kiddo team            
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container className="gridWrapContainer" direction="row">
+        <Grid item xs={11} className="picWrap" direction="row">
+        <h2>Company Statistics</h2>
+        </Grid>
+      </Grid>
+
+      <Grid container className="formWrapContainer" direction="row">
+        <Grid item xs={11} className="formWrap" direction="row">
+      <form className="form" action="">
+        <label>"Contact Kiddo about becoming an Investor."<br/></label>
+        <input type="text" placeholder="enter message here"/>
+        <button>Message Input</button>
+      </form>
+        </Grid>
+      </Grid>
+
+    </div>
+  );
+};
+
+export default InvestorPortal;
--- client/src/components/support/CustomerServicePortal.jsx
+++ client/src/components/support/CustomerServicePortal.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import './CustomerServicePortal.css'
+import React from "react";
+import "./CustomerServicePortal.css";
+import { Grid } from "@mui/material";
+
 const CustomerServicePortal = () => {
   return (
-    <div>CustomerServicePortal</div>
-  )
-}
+    <div>
+      <Grid container className="gridWrapContainer" direction="row">
+        <Grid item xs={10} className="gridDBListContainer" direction="column">
+          <h1>Investor Data</h1>
+        </Grid>
+        <Grid item xs={10} className="gridDBListWrap" direction="column">
+          Company Database list
+          <Grid item xs={10} className="gridCompanyListWrap" direction="row">
+            <ul>
+              <li>Company {1 + 0}</li>
+              <li>Company {2 + 0}</li>
+              <li>Company {3 + 0}</li>
+              <li>Company {4 + 0}</li>
+              <li>Company {5 + 0}</li>
+              <li>Company ...</li>
+            </ul>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default CustomerServicePortal
+export default CustomerServicePortal;
