10b8e12ae2e7d5729f6c104ce83615c41b6dcccd
Kyle Ferguson <ferguson.kd@gmail.com>
2022-08-05 13:51:51 -0400

    investors link conditionals added

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import image6 from "../../assets/image-6.png";
+import InvestorPortal from "../investor/InvestorPortal";
+import { display } from "@mui/system";
+
 const Login = () => {
+  const [logged, setLogged] = useState(false);
+
+  const checkLog = () => {
+    if (logged === true) {
+      return <InvestorPortal/>
+  } else {
+    return '<h1>"You did it wrong"</h1>'
+  }}
+
+  const handleSubmit = async (e) => {
+    let email = e.target[0].value;
+    let password = e.target[1].value;
+    console.log(e);
+
+    e.preventDefault();
+    fetch("http://localhost:8000/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+
+      //* DIVE PLS
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(email, password, data)
+        if (data.auth) {
+          setLogged(true), 
+          <Link to='/investors'/>
+              } else {
+          "we out"
+        }
+        console.log(data)
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="login">
       <h1>Kiddo</h1>
@@ -11,10 +55,9 @@ const Login = () => {
       </div>
 
       <div className="form-container">
-        <form method="post" action="http://localhost:8000/signin">
+        <form onSubmit={handleSubmit}>
           <label>Email</label>
           <input type="text" name="email" placeholder="Enter your email" />
-
           <label>Password</label>
           <input
             type="text"
@@ -26,6 +69,7 @@ const Login = () => {
         <button>Register</button>
         <button>Admin Login</button>
       </div>
+      {checkLog}
     </div>
   );
 };
