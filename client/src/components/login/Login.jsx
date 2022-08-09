a66ef2aada61f33e3428e1e03e41ef1d76b94e77
Kyle Ferguson <ferguson.kd@gmail.com>
2022-08-09 09:16:18 -0400

    register grid added

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
 
   return (
     <Grid container className="loginGrid">
-      <Grid item className="banner" xs={12}>
+      <Grid container className="banner" xs={12}>
         <Grid item className="imageOne" width="50%" xs={9}>
           <img src={image6} id="img7" alt="ads image" width="100%" />
         </Grid>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,48 +1,126 @@
-import React from 'react'
-import './Register.css'
+import React from "react";
+import "./Register.css";
+import image6 from "../../assets/image-6.png";
+import { Grid } from "@mui/material";
+
 const Register = () => {
-  
-  
   return (
-
-  
-
-    <div className='form-container'>
-    <form method="POST" action="http://localhost:8000/signup/submit">
-        <div className='register'>
-    <h1>Kiddo</h1>
-   <h1>Register</h1>
-<h1>Join Our Mission!</h1>
-</div>
-     
-      <label>First Name</label>
-      
-      <input type="text" name="firstName" placeholder='Enter your first name'/>
-      <label>Last Name</label>
-      <input type="text" name="lastName" placeholder='Enter your last name'/>
-      <label>Company Name</label>
-      <input type="text" name="business" placeholder='Enter company name'/>
-      <label>Email</label>
-      <input type="text" name="email" placeholder='Enter your email'/>
-      <label>Country</label>
-      <input type="text" placeholder='Enter your country'/>
-      <label>State</label>
-      <input type="text" placeholder='Enter your state'/>
-      <label>Address</label>
-      <input type="text" name="address" placeholder='Enter your address'/>
-      <label>Zipcode:</label>
-      <input type="text" placeholder='Enter your zipcode'/>
-    <label>Password</label>
-      <input type="text" name="password" placeholder='Enter your password'/>
-      <label>Re-Enter Password</label>
-      <input type="text" placeholder='Re-Enter your password'/>
-
-      <button>Register</button>
+    <Grid container className="regContainer" xs={12}>
+      <Grid container className="banner">
+          <h1>Register With Kiddo To Join Our Mission!</h1>
+          <Grid item className="imageOne" width="50%" xs={9}>
+            <img src={image6} id="img7" alt="ads image" width="100%" />
+          </Grid>
+        </Grid>
+      <form method="POST" action="http://localhost:8000/signup/submit">
+        <Grid container className="regForm" md={12} xs={12}>
+          <Grid container className="field" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              First Name
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Enter your first name"
+              />
+            </Grid>
+          </Grid>
+          <Grid container className="field" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Last Name
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter your last name"
+              />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Company Name
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input
+                type="text"
+                name="business"
+                placeholder="Enter company name"
+              />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Email
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input type="text" name="email" placeholder="Enter your email" />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Country
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input type="text" placeholder="Enter your country" />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              State
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input type="text" placeholder="Enter your state" />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Address
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input
+                type="text"
+                name="address"
+                placeholder="Enter your address"
+              />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              ZIP
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input type="text" placeholder="Enter your zipcode" />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Password
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input
+                type="text"
+                name="password"
+                placeholder="Enter your password"
+              />
+            </Grid>
+          </Grid>
+          <Grid container className="first" md={6} xs={12}>
+            <Grid item className="label" md={5} xs={10}>
+              Re-Enter Password
+            </Grid>
+            <Grid item className="input" md={5} xs={10}>
+              <input type="text" placeholder="Re-Enter your password" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className="button" xs={12}>
+          <button>Register</button>
+        </Grid>
       </form>
+    </Grid>
+  );
+};
 
-      </div>
-      
-  )
-}
-
-export default Register
+export default Register;
