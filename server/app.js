d0228ad09f79ae084cc3bf8e6fddabc10663ccb7
ngardam <ngardam@gmail.com>
2022-08-27 11:00:26 -0400

    whitelist added to cors options

--- server/app.js
+++ server/app.js
@@ -5,16 +5,31 @@ const app = express();
 const mainRoutes = require("./controllers/mainRoutes");
 const forgotPassword = require("./controllers/forgotPassword")
 
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8000', 'https://kiddo-investors.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-
+    
     app.get('*', function ( req , res ) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
 
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(mainRoutes)
 // app.use(forgotPassword)
