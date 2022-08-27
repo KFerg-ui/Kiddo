b5ae8bb18c001ce5af7bec5bc3e5ed69a79218f5
ngardam <ngardam@gmail.com>
2022-08-27 10:53:06 -0400

    logic in app.js that changes the directories of the static server and index.html to be loaded, if in production environment

--- server/app.js
+++ server/app.js
@@ -5,6 +5,15 @@ const app = express();
 const mainRoutes = require("./controllers/mainRoutes");
 const forgotPassword = require("./controllers/forgotPassword")
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function ( req , res ) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    })
+}
+
+
 app.use(cors())
 app.use(express.json())
 app.use(mainRoutes)
