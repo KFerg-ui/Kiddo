6da98dc1c961c3f6375bbf7c734b28197aceeabb
ngardam <ngardam@gmail.com>
2022-08-27 11:53:38 -0400

    changed client build paths in app.js

--- server/app.js
+++ server/app.js
@@ -22,10 +22,10 @@ const corsOptions = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join('/app/client/build')));
     
     app.get('*', function ( req , res ) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join('app/client/build', 'index.html'));
     })
 }
 
